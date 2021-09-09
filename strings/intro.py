7ac44efe70adc3a8e0404622396e221219e0ed77
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2021-09-09 15:00:23 +0000

    more classwork

---
+++ strings/intro.py
@@ -0,0 +1,18 @@
+s = "hello"
+s2 = 'hello'
+s3 = """
+multilines with 
+the triples quotes
+""""
+
+s4 = "hello/nworld/n"
+print(s)
+print(s2)
+print(s3)
+print(s4)
+
+print(s[2])
+
+print("hello" * 2)
+long_string = "this is a long string with stuff"
+print(long_string[3:10])
