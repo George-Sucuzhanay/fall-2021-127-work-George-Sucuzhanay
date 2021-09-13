2bf5324141350975b5a677e7b8692b12801be7cd
George <georgesucuzhanay100@gmail.com>
2021-09-13 10:19:10 -0400

    add classwork notes and print statement

--- classwork/09-13-classwork.py
+++ classwork/09-13-classwork.py
@@ -1,1 +1,11 @@
 # Git
+# Setting up ssh key
+# Do:
+# ssh-keygen in the terminal
+# cat .ssh/id_rsa.pub
+# git clone git@github.com:hunter-classes/fall-2021-127-work-George-Sucuzhanay.git (repo ssh)
+
+# Notes:
+# copies repo from Github to local machine
+#
+print("I made a change")
