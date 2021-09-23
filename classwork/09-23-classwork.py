d08cece0bb86f5151ee15c723016ea6c9899bce1
George <georgesucuzhanay100@gmail.com>
2021-09-23 10:05:31 -0400

    new notes

---
+++ classwork/09-23-classwork.py
@@ -0,0 +1,16 @@
+def piglatinify(word):
+    """
+    input: a single word
+    returns: the word transformed into piglatin
+    
+    Notes: if the first letter is a vowel, add "ay" to the end
+        otherwise move first to end and add "ay" to end
+        
+    """
+    pass
+
+def test():
+    pass
+
+if __name__=="__main__":
+    pass
