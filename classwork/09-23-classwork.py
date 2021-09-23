2fe52717b587e47e4ba46f290ab535f9f94ecd6e
George <georgesucuzhanay100@gmail.com>
2021-09-23 10:38:25 -0400

    classwork exercise

--- classwork/09-23-classwork.py
+++ classwork/09-23-classwork.py
@@ -1,3 +1,4 @@
+import random
 def piglatinify(word):
     """
     input: a single word
@@ -7,10 +8,33 @@ def piglatinify(word):
         otherwise move first to end and add "ay" to end
         
     """
-    pass
+    # separate word into first and rest
+    first = word[0]
+    rest = word[1:]
+    # ABCD      A is the first and BCD is the rest
+    # if first is a vowel add ay
+    VOWELS = "aeiou"
+    if first in VOWELS:
+        encoded = word + "ay"
+    # otherwise move first to last and add ay
+    else:
+        encoded = rest + first + "ay"
+    
+    return encoded
+
+    # debugging is 10 times harder than write your code
+    # if working on a program : what is the minimal amount of people that work on a programming project
+    # you and you two weeks from now   wowowowowowowowo!!!!!!
+    
 
 def test():
-    pass
+    s = "hello"
+    print(s,piglatinify(s));
+    s = "able"
+    print(s,piglatinify(s));
+    s = "bob"
+    print(s,piglatinify(s));
+
 
 if __name__=="__main__":
-    pass
+    test()
