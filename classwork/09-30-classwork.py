ecd49fb1ff36b9b943a2fba20261f1418be8c98a
George <georgesucuzhanay100@gmail.com>
2021-09-30 09:54:47 -0400

    changing filenames

---
+++ classwork/09-30-classwork.py
@@ -0,0 +1,4 @@
+# Renaming file names in the terminal
+# git mv oldname newname
+# git commit
+# git push
--- hw-05-lists/hw-05.py
+++ hw-05-lists/hw-05.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Create a file named hw05.py and in it write the following functions (and when the program is run, they should be tested and should return and pritn their output)
 
 # 1) Write a function keep-odds that that will take a list of integers as a parameter. 
@@ -13,11 +15,40 @@ def keepodds(num):
             oddlist.append(i) 
     return oddlist
     
+def wordsInSentence(sentence):
+    count = 0;
+    for i in sentence: 
+      if len(i) == 5: 
+        count += 1
+    return count
+
+
+# 11) Sum all the elements in a list up to but not including the first even number.
+def sumUntilEven(listNum):
+    sumCount = 0
+    for i in listNum:
+        if i % 2 != 0:
+            sumCount += i
+        else:
+            return sumCount
+    return sumCount
+
+# 12) Count how many words occur in a list up to and including the first occurrence of the word “sam”.
+def countWords(listNames):
+    count = 0
+    for i in listNames:
+        count +=1
+        if i == "sam":
+            break
+    return count
 
 def tests():
     # add tests here
     print(keepodds([1,2,3,4,5,6,7]))
     print(keepodds([10,11,12,13,14]))
+    print(wordsInSentence(["how", "good", "wonderful", "excited", "joyful"]))
+    print(sumUntilEven([1,3,5,7,9,10]))
+    print(countWords(["momama","dad","son","cousin", "sam", "great"]))
     
 
 if __name__== "__main__":
