3cdee062210febb7eb4571f85214cf14f721d995
George <georgesucuzhanay100@gmail.com>
2021-10-07 10:40:38 -0400

    project files and classwork

---
+++ project-01-madlibs/madlibs.py
@@ -0,0 +1,16 @@
+from typing_extensions import Concatenate
+
+
+# Possibles Ideas:
+# - creating a huge string and then concatenate
+# -  must use for loops
+# - possible look how to capitalize the first word of a sentence
+
+
+# - extra of user choice 
+
+# <-------------------------------------------------------------------------------------->
+# MAKE SURE TO MAKE MORE THAN ONE COMMITS SHOWING YOU PROGRESSIVELY WORKED ON THE PROJECT
+# POINTS DEDUCTED!
+# <-------------------------------------------------------------------------------------->
+
