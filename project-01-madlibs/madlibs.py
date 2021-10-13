aa9f5da06368e2716d1f1d594ff543bdf058535f
George <georgesucuzhanay100@gmail.com>
2021-10-13 11:25:59 -0400

    assigned 3 functions

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,6 +1,3 @@
-from typing_extensions import Concatenate
-
-
 # Possibles Ideas:
 # - creating a huge string and then concatenate
 # -  must use for loops
@@ -13,4 +10,14 @@ from typing_extensions import Concatenate
 # MAKE SURE TO MAKE MORE THAN ONE COMMITS SHOWING YOU PROGRESSIVELY WORKED ON THE PROJECT
 # POINTS DEDUCTED!
 # <-------------------------------------------------------------------------------------->
+def randomVerb():
+    verbs=['ate','walked','slept']
 
+def randomNoun():
+    nouns=['dog','hammer','cat','car','frog']
+
+def madlib(randomNoun, randomVerb):
+    pass
+
+# When run, it should print out at least two original sentences each followed by two different madlab outputs for each.
+# These sentences can be declared as multiline (triple quote) strings in your program.
