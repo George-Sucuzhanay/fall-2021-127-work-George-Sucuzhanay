c8e0b0551a7fdbecab619db40ef7f5ecc5aa7d05
George <georgesucuzhanay100@gmail.com>
2021-10-17 00:25:31 -0400

    new function to randomize randomverb and random noun

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,10 +1,11 @@
+"""
+George Sucuzhanay
+Madlibs Project
+"""
 # Possibles Ideas:
 # - creating a huge string and then concatenate
 # -  must use for loops
 # - possible look how to capitalize the first word of a sentence
-
-
-# - extra of user choice 
 
 # <-------------------------------------------------------------------------------------->
 # MAKE SURE TO MAKE MORE THAN ONE COMMITS SHOWING YOU PROGRESSIVELY WORKED ON THE PROJECT
@@ -15,18 +16,18 @@ from random import randrange
 
 def randomVerb():
     verbs = ['ate','walked','slept','behave', 'belong', 'hold']
-    i = randrange(len(verbs))
+    i = randrange(0,len(verbs))
     item = verbs[i]
-    return verbs[i]
+    return item
 
 # print(randomVerb())
 
 def randomNoun():
     nouns = ['dog','hammer','cat','car','frog', 'horse', "John", 'dragon', 'home']
-    i = randrange(len(nouns))
+    i = randrange(0,len(nouns))
     item = nouns[i]
-    return nouns[i]
-    
+    return item
+ 
 # print(randomNoun())
 
 def madlib(randomNoun, randomVerb):
@@ -34,13 +35,29 @@ def madlib(randomNoun, randomVerb):
     myStoryLine2 = "Piggie tells Gerald that when she returns to make sure to get"
     return myStoryLine1 + myStoryLine2
 
-print(madlib(randomNoun(), randomVerb()))
-
-
-# from random import randrange
-
-# movie_list = ['The Godfather', 'The Wizard of Oz', 'Citizen Kane', 'The Shawshank Redemption', 'Pulp Fiction']
-
+# print(madlib(randomNoun(), randomVerb()))
 
 # When run, it should print out at least two original sentences each followed by two different madlab outputs for each.
 # These sentences can be declared as multiline (triple quote) strings in your program.
+
+# Practice Function:
+def working(myStoryLineEx):
+    testing = myStoryLineEx.split()
+    # print(testing)
+    newList = []
+    for items in testing:
+        if (items == '<noun>' or items == '<noun>,' or items == '<noun>.'):
+            print("this conditional is true and its a noun")
+            i = randomNoun()
+            newList.append(i)
+            # print(items)
+        elif(items == '<verb>' or items == '<verb>,' or items == '<verbs>.'):
+            print("this conditonal is working and its a verb")
+            i = randomVerb()
+            newList.append(i)
+        else:
+            newList.append(items)
+    return newList
+
+myStoryLineEx = "One day, Gerald and Piggie decided to go on a <noun>. Gerald and Piggie began to <verb>. Gerald asks if they need to pack <noun>."
+print(working(myStoryLineEx))
