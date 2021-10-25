3f1b6175d56bf74ca67997fef61cdcbc64c951c8
George <georgesucuzhanay100@gmail.com>
2021-10-25 10:55:23 -0400

    today's classwork

--- classwork/10-25-classwork2.py
+++ classwork/10-25-classwork2.py
@@ -43,6 +43,38 @@ def mode2(L):
 
     return mode_so_far
 
+def modefast(L):
+    # first make a list to hold the tallies
+    # since our data values are between 0 and 99 inclusive
+    # we need a list of 100 spaces (0 through 99)
+    tallies = [0] * 100
+
+    # alternate ways to do the previous line:
+    # tallies = [0 for x in range(100)]
+    # or
+    # tallies = []
+    # for i in range(100):
+    #   tallies.append(0)
+
+    # second, go through L and add to the tallies
+    for item in L:
+        tallies[item] = tallies[item] + 1
+
+    # finally, find the index of the largest entry
+    # in tallies
+    largeindexsofar = 0
+    for i in range(len(tallies)):
+        if (tallies[i] > tallies[largeindexsofar]):
+            largeindexsofar = i
+    # largeindexsofar has the mode
+    return largeindexsofar
+
+    # big o notation
+    # n + c + n + c = 2n + 2c => n
+    # remember to know if you are looping through the indices or the value of them
+    # this is a common bug issue with Python developers
+
+
 def sumeven(l):
     s = 0
     for item in l:
@@ -93,4 +125,11 @@ if __name__=="__main__":
 # a loop within a loop is => n^2
 # a loops below another loop => 2n
 # a loop within a loop within a loop n^3
+
+# 10/25/2021
+# Making tallies of count by using an array
+# NYC Mayor Election analogy
+
+# if a solution is not outputs like you want it entirely
+# change your data to adjust your output
 
