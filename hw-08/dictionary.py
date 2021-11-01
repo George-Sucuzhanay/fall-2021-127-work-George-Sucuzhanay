445f48c040d297d70fa006a70112157936086a84
George <georgesucuzhanay100@gmail.com>
2021-10-31 22:58:01 -0400

    dictionary1 and dictionary2 excersies completed

--- hw-08/dictionary.py
+++ hw-08/dictionary.py
@@ -1,13 +1,28 @@
 # 12.1 
-# Write a program that allows the user to enter a string. It then prints a table of the letters of the alphabet in alphabetical order which occur in the string together with the number of times each letter occurs. Case should be ignored. A sample run of the program might look this this:
+# Write a program that allows the user to enter a string. 
+# It then prints a table of the letters of the alphabet in alphabetical order which occur in the string 
+# together with the number of times each letter occurs. 
+# Case should be ignored. 
 
-sentence = input("Please enter a string: ")
+word = input("Please enter a string: ")
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","w"]
-
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","x"]
 
 myCountofLetter = {}
 
-for letter in sentence:
-    if letter in alphabet:
-        pass
+def example(alphabet, word):
+  for char in word:
+    for letters in alphabet:
+      if char == letters:
+        if letters in myCountofLetter:
+          myCountofLetter[letters] = myCountofLetter[letters] + 1
+        else:
+          myCountofLetter[letters] = 1
+  return myCountofLetter
+
+print(example(alphabet, word))
+
+keys = myCountofLetter.keys()
+count = myCountofLetter.values()
+for keyValue in sorted(keys):
+  print(keyValue, myCountofLetter[keyValue])
--- hw-08/dictionary2.py
+++ hw-08/dictionary2.py
@@ -1,3 +1,15 @@
 # 12.4
 # What is the longest word in Alice in Wonderland? How many characters does it have?
 
+word = "Alice in Wonderland"
+newArr = word.split()
+
+def bigWords(newArr):
+    largestWord = ""
+    for word in newArr:       
+        if len(largestWord) < len(word):
+            largestWord = word
+    lengthOfLongWord = len(largestWord)
+    return "The longest word is " + largestWord + ". And it has " + str(lengthOfLongWord) + " characters." 
+
+print(bigWords(newArr))
