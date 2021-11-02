ab7442c09b0535d9abe9eb1eab22afeea6bfa111
George <georgesucuzhanay100@gmail.com>
2021-11-02 08:33:53 -0400

    classwork in CS

--- classwork/10-28-classwork3.py
+++ classwork/10-28-classwork3.py
@@ -1,9 +1,19 @@
 import random
 
+# nums_occured
+
+def find_max(l):
+    m = l[0]
+    for item in l:
+        if item > m:
+            m = item
+    return m
+
+
 data = [random.randrange(100) for x in range(100)]
 
 nums = {}
-# here we're counting how many times each number apper
+# here we're counting how many times each number appear
 # in data but the first time we see a numebr it will give an error
 # if we try to access it by using an if statement
 
@@ -20,11 +30,19 @@ nums = {}
 # even better way
 
 for number in data:
-    # if number isn't in nums then set it to 1
+    # if number isn't in nums then set it to 0
     # but if it is, this line will do nothing
-    nums.setdefault(number,1)
+    nums.setdefault(number,0)
 
     # when we get here, nums[number] WILL exist (becuase
     # of the above line or maybe an earlier iteration fo the loop
     # so we can just add 1 to the count
     nums[number] = nums[number] + 1
+
+v = nums.values()
+m = find_max(list(v))
+
+print(nums)
+
+largest_values = [x for x in nums.keys() if nums[x] == m]
+
