a2d6f7e1fe9da88cd9d102c32f24771fc424ba4e
George <georgesucuzhanay100@gmail.com>
2021-11-04 10:31:24 -0400

    read piratedat and input.txt textfiles

---
+++ project-02-speak/speak.py
@@ -0,0 +1,18 @@
+# Task:
+# Create a program name speak.py. This program will read it’s input from a file named input.txt and will convert it to piratespeak and print out the result.
+
+# Your program will take an text file named input.txt and read it in and use the dictionary (described below) to translate the contents of input.txt to pirate-speak. Your program should print out the results.
+
+
+# make more than 1 commit 
+
+# use a dictionary of pirate traslations
+# the story line must be in a input.txt textfile
+
+# Extra:
+# use a data text file and import it
+
+
+
+text = open("project-02-speak/input.txt").read()
+translation = open("project-02-speak/pirate.dat").read()
