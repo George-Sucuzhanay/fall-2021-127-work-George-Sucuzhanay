39bff3c9f1932cf84df73361886886269aeecd6b
George <georgesucuzhanay100@gmail.com>
2021-11-18 10:51:55 -0500

    classwork and sentiment func

--- classwork/11-08-classwork.py
+++ classwork/11-08-classwork.py
@@ -57,32 +57,63 @@ def alter(bag,stopwordfilename):
             newbag[word]=bag[word]
     return newbag
 
+def count_words(bag):
+    # sum = 0
+    # for key in bag.keys():
+    #     sum = sum + bag[key]
+    # return sum
+
+    # shorter list comprehension version
+    return sum([bag[k] for k in bag.keys()])
+
 def sentiment(bag,wordlistfile):
     # first, read in wordlistfile in a list
+    sentiwords = open(wordlistfile,encoding="Latin-1").read().split()
 
     # Version 1: calculate the total number of words in bag
-    # Version 2: calculate the # of different words in bag
+    total_words = count_words(bag)
 
+    
     # Version 1: calculate the number of words in bag that are in wordlistfile
-    # Version 2: calculate the number of different words in bag tht are in wordlistfile
+    # sum = 0
+    # for k in bag.keys():
+    #     if k in sentiwords:
+    #         sum = sum + bag[k]
+    # print(sum)
+
+    total_sentwords = sum([bag[k] for k in bag.keys() if k in sentiwords])
 
     # Version 1: return the number from wordlistfile / total words
-    # Version 2: return the same ratio but with v2 numbers
+    return total_sentwords / total_words
 
-    pass
+def sentiwords_two(bag,wordlistfile):
+    sentiwords = open(wordlistfile,encoding="Latin-1").read().split()
+    # Version 2: calculate the # of different words in bag
+    total_words = len(bag.keys())
+
+    # Version 2: calculate the number of different words in bag tht are in wordlistfile
+    total_sentwords = sum([1 for k in bag.keys() if k in sentiwords])
+
+    # Version 2: return the same ratio but with v2 numbers
+    return total_sentwords / total_words
 
 def main():
     load_bow("classwork/chapter1.txt")
 
-    k = list(bag.keys())
-    v = list(bag.values())
-    m = find_max(v)
+    # k = list(bag.keys())
+    # v = list(bag.values())
+    # m = find_max(v)
     # print(m)
     # find the most frequent word
-    position = v.index(124)
+    # position = v.index(124)
     # print(k[position])
     # print(get_words_more_frequent(bag,25))
     # print(get_words_more_frequent_dict(bag,25))
-    print(alter(bag,"classwork/stopwords.txt"))
+    # print(alter(bag,"classwork/stopwords.txt"))
+    print(sentiment({'and' : 5, "wow": 6, "dog":20, "abound": 20, "abounds" :10, "abundance":10},"classwork/positive.txt"))
+    print(sentiwords_two({'and' : 5, "wow": 6, "dog":20, "abound": 20, "abounds" :10, "abundance":10},"classwork/positive.txt"))
+
+    # encoding:
+    # print(count_words({'and' : 5, "wow": 6}))
 if __name__ == "__main__":
     main()
