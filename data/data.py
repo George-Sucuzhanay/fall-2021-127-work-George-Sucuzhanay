367f854a137a93ee7b08d58532877e26aa9982be
George <georgesucuzhanay100@gmail.com>
2021-12-02 13:03:57 -0500

    manipulating csv data and printing borough names

--- data/data.py
+++ data/data.py
@@ -1,27 +1,32 @@
+# CSV Data Project
+# Author: George Sucuzhanay
+# Using NYC Open Data Unbanked Dataset: 
+# https://data.cityofnewyork.us/Business/Where-Are-the-Unbanked-and-Underbanked-in-New-York/v5w4-adxa
+
 import csv
 
-def clean(line):
-    """
-    takes in a string and returns a cleaned string.
-    1. convert to lower case
-    2. only keep letters and tab,space,newline
-    """
-    line = line.lower()
-    letters_to_keep = 'abcdefghijklmnopqrstuvwxyz\t \n'
-    # result = ""
-    # for letter in line:
-    #     if letter in letters_to_keep:
-    #         result = result + letter
-    result = [x for x in line if x in letters_to_keep]
-    cleaned = "".join(result)
-    return cleaned
+# def clean(line):
+#     """
+#     takes in a string and returns a cleaned string.
+#     1. convert to lower case
+#     2. only keep letters and tab,space,newline
+#     """
+#     line = line.lower()
+#     letters_to_keep = 'abcdefghijklmnopqrstuvwxyz\t \n'
+#     # result = ""
+#     # for letter in line:
+#     #     if letter in letters_to_keep:
+#     #         result = result + letter
+#     result = [x for x in line if x in letters_to_keep]
+#     cleaned = "".join(result)
+#     return cleaned
 
-def remove_stop_words(line):
-    f = open("classwork/stopwords.txt")
-    stopwords = f.read().split()
-    result = [x for x in line.split() if x not in stopwords]
-    line = " ".join(result)
-    return line
+# def remove_stop_words(line):
+#     f = open("classwork/stopwords.txt")
+#     stopwords = f.read().split()
+#     result = [x for x in line.split() if x not in stopwords]
+#     line = " ".join(result)
+#     return line
 
 def add_statement(index,key,statement):
     for word in statement.split():
@@ -31,16 +36,20 @@ def add_statement(index,key,statement):
     return index
 f = open("data/underbanked.csv", encoding='Latin-1')
 
+# Create a dictionary where
+    # 1) Keys are the boroughs of NYC
+    # 2) Values are the percentage of unbanked in such borought
 reader = csv.DictReader(f)
 people = {}
 index = {}
 for item in reader:
+    print(item['Sub Boro Name'])
     key = item['Sub Boro Name']
-    people[key] = item
-    statement = clean(item['Underbanked 2011'])
-    # we might not want to remove the stop words
-    statement = remove_stop_words(statement)
-    index = add_statement(index,key,statement)
+    # people[key] = item
+    # statement = clean(item['Underbanked 2011'])
+    # # we might not want to remove the stop words
+    # statement = remove_stop_words(statement)
+    # index = add_statement(index,key,statement)
 
 # 1: ABCD
 # 2: BCFG
@@ -75,19 +84,32 @@ def s_and(index,words):
 #     if len(index[key]) > 50:
 #         print(key,len(index[key]))
 
-print('father', len(index['father']), index['father'])
-print('mother', len(index['mother']), index['mother'])
-print()
-result = s_or(index, ['mother','father'])
-print('both',len(result),result)
-print()
-print()
-result = s_and(index,['mother','father'])
-print(result)
+
+
+
+
+
+
+
+
+# print('father', len(index['father']), index['father'])
+# print('mother', len(index['mother']), index['mother'])
+# print()
+# result = s_or(index, ['mother','father'])
+# print('both',len(result),result)
+# print()
+# print()
+# result = s_and(index,['mother','father'])
+# print(result)
+
+
+
+
+
 
 # Find the borough with the most underbanked will need the mode func
     # check to see it is also the borough with the highest percantage of foriegn born
-    
+
 # Find the borough with highest median income
     # compare it with the borough with the lowest median income
     # compare the underbanked perctanges of these 2 boroughs
