bf37cffb32519f6bbec360d9a285e5ae4d091d8c
George <georgesucuzhanay100@gmail.com>
2021-12-09 10:06:09 -0500

    classwork of c++

---
+++ classwork/12-09-classwork.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+// library with advanced tools ???? similar to Python
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    // To run you do ./compliername
+}
